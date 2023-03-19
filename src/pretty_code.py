10e8fcaf391350b41748b7674c52e49cfac24a1f
e-tornike <20404466+e-tornike@users.noreply.github.com>
2023-03-19 22:11:06 +0100

    first commit

---
+++ src/pretty_code.py
@@ -0,0 +1,21 @@
+import os
+
+from pre_commit_example import random_function
+
+
+def function(
+    arg1: str,
+    arg2: str,
+    arg3: int,
+    arg4: int,
+    arg5: float,
+    arg6: float,
+    arg7: bool,
+    arg8: bool,
+) -> str:
+
+    path = os.path.join(arg1, arg2, "abc", "efg", "hij")
+    random_function(path)
+
+    return "Function was called."
+
