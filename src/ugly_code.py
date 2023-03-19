9cfa19bda9b2bba5b23fb5b40e032afe1bdee8ae
e-tornike <20404466+e-tornike@users.noreply.github.com>
2023-03-19 22:25:54 +0100

    remove lines

--- src/ugly_code.py
+++ src/ugly_code.py
@@ -3,8 +3,6 @@ import os
 from pathlib import Path
 from . import random_function
 
-
-
 def function(arg1: str, arg2: str, arg3: int, arg4: int, arg5: float, arg6: float, arg7: bool, arg8: bool) -> str:
     
     path = os.path.join(arg1, arg2, 'abc', "efg", "hij")
